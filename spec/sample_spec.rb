dd01b990641091914b676353e6351820ee9228c3
Leon Gersing <leongersing@gmail.com>
2011-07-01 19:18:14 -0400

    contrived examples FTW!

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 
-MyConfig = Configulations.new
-MyConfig.root = "/usr/local/bin/awesome"
-
+# see the spec_helper to see a crazy MyConfig class.
 describe "Sample use" do
   it "just works." do
     MyConfig.should_not be_nil
