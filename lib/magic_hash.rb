bb198b0767cb0a7bfcd5cdad16eae5b7fb90089d
Leon Gersing <leongersing@gmail.com>
2011-07-07 09:44:21 -0400

    This commit is the result of a pairing session with al2o3cr. Who deserves credit that is not on the commit.

--- lib/magic_hash.rb
+++ lib/magic_hash.rb
@@ -5,52 +5,23 @@ module MagicHash
   end
 
   def objectify
-    self.make_keys_valid_message_aliases
-    self.hash_all_keys
-    self
-  end
-
-  def make_keys_valid_message_aliases
     tmp = MagicHash.new
     self.each_pair do |key, value|
       new_key = key.is_a?(Symbol) ? key : key.gsub(/\s|-/,"_").strip.to_sym
       if(value.is_a? Hash)
         value.extend(MagicHash) unless value.respond_to? :objectify
-        tmp[new_key] = value.make_keys_valid_message_aliases
+        tmp[new_key] = value.objectify
       elsif(value.is_a? Array)
         tmp[new_key] = value.map do |val|
           if val.is_a?(Hash)
             val.extend(MagicHash) unless val.respond_to? :objectify
-            val.make_keys_valid_message_aliases
+            val.objectify
           else
             val
           end
         end
       else
         tmp[new_key] = value
-      end
-    end
-    self.replace(tmp)
-  end
-
-  def hash_all_keys
-    tmp = MagicHash.new
-    self.each_pair do |k, v|
-      if v.is_a? Hash
-        v.extend(MagicHash) unless v.respond_to? :objectify
-        tmp[k.to_sym]= v.hash_all_keys
-      elsif v.is_a? Array
-        v = v.map do |value|
-          if value.is_a?(Hash.class)
-            value.extend(MagicHash) unless value.respond_to? :objectify
-            value.hash_all_keys
-          else
-            value
-          end
-        end
-        tmp[k.to_sym]= v
-      else
-        tmp[k.to_sym]= v
       end
     end
     self.replace(tmp)
